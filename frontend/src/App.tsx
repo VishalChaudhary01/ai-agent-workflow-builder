554b5a4c26e2715458b161a9ea06d43e5a2f3ffe
Vishal Chaudhary <vishalchaudhary8832@gmail.com>
2026-03-01 11:13:51 +0530

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <h2>Hi THere</h2>
+      <Button>Click me</Button>
+    </div>
+  );
+}
